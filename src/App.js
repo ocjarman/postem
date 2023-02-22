d999c2d3edebd86e2d3694bf8b0c8c14c21cc703
ocjarman <ocjarman@gmail.com>
2023-02-21 20:58:57 -0500

    stuck point on querying apollo server

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import Profile from "./components/Profile";
+import { useQuery, gql } from "@apollo/client";
+import DisplayLocations from "./components/DisplayLocations";
+
 function App() {
   return (
     <div className="App">
       <LoginButton />
       <LogoutButton />
       <Profile />
+      <DisplayLocations />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,18 +3,46 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const client = new ApolloClient({
+  uri: "https://plankton-app-cxczj.ondigitalocean.app/",
+  // uri: "https://flyby-router-demo.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
+
+console.log(client);
+client
+  .query({
+    query: gql`
+      query me {
+        id
+        firstName
+        lastName
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
 root.render(
-  <Auth0Provider
-    domain="dev-bgwfnefdow6rcoct.us.auth0.com"
-    clientId="FvuF3W9Xp4zLL0YmDLWLuHB6EWjKJN7N"
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-      audience: "https://dev-bgwfnefdow6rcoct.us.auth0.com/api/v2/",
-      scope: "read:current_user update:current_user_metadata",
-    }}
-  >
-    <App />
-  </Auth0Provider>
+  <ApolloProvider client={client}>
+    <Auth0Provider
+      domain="dev-bgwfnefdow6rcoct.us.auth0.com"
+      clientId="FvuF3W9Xp4zLL0YmDLWLuHB6EWjKJN7N"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+        audience: "https://dev-bgwfnefdow6rcoct.us.auth0.com/api/v2/",
+        scope: "read:current_user update:current_user_metadata",
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </ApolloProvider>
 );
